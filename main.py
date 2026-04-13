78045d160120c5029ede4d64e538fbe4b5b5c273
Sreekanth EJ <srkhej2004@gmail.com>
2026-04-13 16:18:51 +0530

    added more FastAPI routes and improvements

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
 from fastapi import FastAPI
+from typing import  Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+@app.get('/blog')
+def index(limit=10,published : bool=True,sort:Optional[str] =None):
+    #only get 10 published blogs
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data':{f'{limit} blogs from the db'}}
 
-@app.get('/')
-def abc():
-    return {'data': {'name': 'Sreekanth'}}
 
-@app.get('/about')
-def about():
-    return {'data': {'page': 'about'}}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'all unpublished blog'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': id}
+
+
+
+@app.get('/blog/{id}/comments')
+def comments(id: int):
+    #fetch comments of blog with id = id
+    return {'data':['1','2']}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {'data':f"Blog is created with title as {request.title}"}
