fabb8146034ab7e368c479bfbb268c1f03b9cbd6
Sreekanth EJ <srkhej2004@gmail.com>
2026-04-15 17:34:25 +0530

    Add blog folder with project code

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, INTEGER, String
+from .database import Base
+
+
+
+
+class Blog(Base):
+    __tablename__ = 'blogs'
+    id = Column(INTEGER,primary_key = True, index=True)
+    title = Column(String)
+    body = Column(String)
+
