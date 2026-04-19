d0217851e1641167367970a4a174573731873da5
Sreekanth EJ <srkhej2004@gmail.com>
2026-04-19 16:53:11 +0530

    Fixed update API bug and error handling

--- blog/main.py
+++ blog/main.py
@@ -30,7 +30,11 @@ def destroy(id, db:Session= Depends(get_db)):
 
 @app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
 def update(id, request:schemas.Blog, db: Session=Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).update(request)
+    blog = (db.query(models.Blog).filter(models.Blog.id == id))
+    if not blog.first():
+         raise HTTPException(status_code= status.HTTP_404_NOT_FOUND,
+                            detail = f"Blog with the id {id} is not available")
+    blog.update(request)
     db.commit()
     return 'Updated successfully'
 
