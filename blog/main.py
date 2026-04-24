5ed92f825bd18352b85837a631125fb8d148be9d
Sreekanth EJ <srkhej2004@gmail.com>
2026-04-24 15:32:51 +0530

    fixed user schema and hahsing

--- blog/main.py
+++ blog/main.py
@@ -4,7 +4,8 @@ from sqlalchemy.util import deprecated
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
-from passlib.context import CryptContext
+from .hashing import Hash
+from .schemas import ShowUser
 
 app = FastAPI()
 
@@ -67,14 +68,20 @@ def show(id,response: Response, db: Session = Depends(get_db)):
 
 
 
-pwd_cxt = CryptContext(schemes=["bcrypt"],deprecated = "auto")
-
-
-@app.post('/user')
+@app.post('/user',response_model=schemas.ShowUser)
 def create_user(request: schemas.User,db:Session=Depends(get_db)):
-    hashedPassword = pwd_cxt.hash(request.password)
-    new_user = models.User(name = request.name, email = request.email, password = hashedPassword)
+    new_user = models.User(name = request.name, email = request.email, password =Hash.bcrypt
+    (request.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@app.get('/user/{id}',response_model=schemas.ShowUser)
+def get_user(id:int,db:Session=Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with the id {id} is not available")
+    return user
