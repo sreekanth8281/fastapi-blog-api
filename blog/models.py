922ed47cd449d5147d4be20215dc9ba3b25eb0f2
Sreekanth EJ <srkhej2004@gmail.com>
2026-04-26 11:14:28 +0530

    Add relationship between User and Blog models

--- blog/models.py
+++ blog/models.py
@@ -1,22 +1,26 @@
-from sqlalchemy import Column, INTEGER, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from .database import Base
-
+from sqlalchemy .orm import relationship
 
 
 
 class Blog(Base):
     __tablename__ = 'blogs'
-    id = Column(INTEGER,primary_key = True, index=True)
+    id = Column(Integer,primary_key = True, index=True)
     title = Column(String)
     body = Column(String)
+    user_id = Column(Integer,ForeignKey('users.id'))
+
+    creator = relationship('User',back_populates="blogs")
 
 
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(INTEGER,primary_key=True,index=True)
+    id = Column(Integer,primary_key=True,index=True)
     name = Column(String)
     email = Column(String)
     password = Column(String)
 
 
+    blogs = relationship('Blog',back_populates='creator')
